3ae1f28040ef713ac9e681381a3b5ea2dc1953c0
Marco Antonio Islas Cruz <markuz@islascruz.org>
2011-01-31 12:04:13 -0600

    Updated copyright

--- receivers/linux/bl.py
+++ receivers/linux/bl.py
@@ -1,8 +1,31 @@
-# file: rfcomm-server.py
-# auth: Albert Huang <albert@csail.mit.edu>
-# desc: simple demonstration of a server application that uses RFCOMM sockets
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 #
-# $Id: rfcomm-server.py 518 2007-08-10 07:20:07Z albert $
+# This file is part of the Bluetooth Note Controller project
+#
+# Copyright (c) 2006-2009 Marco Antonio Islas Cruz
+#
+# Bluetooth Note Controller is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Bluetooth Note Controller is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# @category  Multimedia
+# @package   Bluetooth Note Controller
+# @author    Marco Antonio Islas Cruz <markuz@islascruz.org>
+# @copyright 2010-211 Marco Antonio Islas Cruz
+# @license   http://www.gnu.org/licenses/gpl.txt
+
+# inspired on the work by Albert Huang <albert@csail.mit.edu>
 
 from bluetooth import *
 import time
